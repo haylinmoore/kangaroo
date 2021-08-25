ef3d7505e6309299e062e5ef64440c193c74907d
Hampton Moore <me@hampton.pw>
2021-08-25 19:57:22 -0400

    Rework handling of TCP redirect

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/gliderlabs/ssh v0.3.3
 	github.com/go-ping/ping v0.0.0-20210506233800-ff8be3320020
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5
 )
--- l2portal.go
+++ l2portal.go
@@ -3,67 +3,102 @@ package main
 import (
 	"io"
 	"log"
+	"net"
 	"os/exec"
+	"strconv"
 	"strings"
 
 	"github.com/gliderlabs/ssh"
 	"github.com/go-ping/ping"
+	gossh "golang.org/x/crypto/ssh"
 )
 
+// direct-tcpip data struct as specified in RFC4254, Section 7.2
+type localForwardChannelData struct {
+	DestAddr string
+	DestPort uint32
+
+	OriginAddr string
+	OriginPort uint32
+}
+
+func customDirectHandler(srv *ssh.Server, conn *gossh.ServerConn, newChan gossh.NewChannel, ctx ssh.Context) {
+	d := localForwardChannelData{}
+	if err := gossh.Unmarshal(newChan.ExtraData(), &d); err != nil {
+		newChan.Reject(gossh.ConnectionFailed, "error parsing forward data: "+err.Error())
+		return
+	}
+
+	dhost := d.DestAddr
+	onL2 := false
+
+	pinger, err := ping.NewPinger(dhost)
+	if err != nil {
+		newChan.Reject(gossh.Prohibited, dhost+" is not accessible")
+		return
+	}
+	pinger.Count = 1
+	pinger.Timeout = 1
+	err = pinger.Run() // blocks until finished
+	if err != nil {
+		newChan.Reject(gossh.Prohibited, dhost+" is not accessible")
+	}
+	stats := pinger.Statistics()
+
+	cmd := exec.Command("ip", "neigh")
+	out, err := cmd.Output()
+	if err != nil {
+		panic(err)
+	}
+	hosts := strings.Split(string(out), "\n")
+	for _, host := range hosts {
+		if strings.Contains(host, stats.IPAddr.IP.String()) {
+			parts := strings.Split(host, " ")
+			if parts[len(parts)-1] == "REACHABLE" || parts[len(parts)-1] == "DELAY" {
+				onL2 = true
+			}
+		}
+	}
+
+	if !onL2 {
+		newChan.Reject(gossh.Prohibited, dhost+" is not on local L2")
+		return
+	}
+
+	dest := net.JoinHostPort(d.DestAddr, strconv.FormatInt(int64(d.DestPort), 10))
+
+	var dialer net.Dialer
+	dconn, err := dialer.DialContext(ctx, "tcp", dest)
+	if err != nil {
+		newChan.Reject(gossh.ConnectionFailed, err.Error())
+		return
+	}
+
+	ch, reqs, err := newChan.Accept()
+	if err != nil {
+		dconn.Close()
+		return
+	}
+	go gossh.DiscardRequests(reqs)
+
+	go func() {
+		defer ch.Close()
+		defer dconn.Close()
+		io.Copy(ch, dconn)
+	}()
+	go func() {
+		defer ch.Close()
+		defer dconn.Close()
+		io.Copy(dconn, ch)
+	}()
+}
 func main() {
 	log.Println("starting ssh server on port 2222...")
 
 	server := ssh.Server{
-		LocalPortForwardingCallback: ssh.LocalPortForwardingCallback(func(ctx ssh.Context, dhost string, dport uint32) bool {
-			log.Println("SESSION")
-			log.Println(dhost, dport)
-
-			onL2 := false
-
-			pinger, err := ping.NewPinger(dhost)
-			if err != nil {
-				panic(err)
-			}
-			pinger.Count = 1
-			pinger.Timeout = 1
-			err = pinger.Run() // blocks until finished
-			if err != nil {
-				return false
-			}
-			stats := pinger.Statistics()
-
-			cmd := exec.Command("ip", "neigh")
-			out, err := cmd.Output()
-			if err != nil {
-				panic(err)
-			}
-			hosts := strings.Split(string(out), "\n")
-			for _, host := range hosts {
-				if strings.Contains(host, stats.IPAddr.IP.String()) {
-					parts := strings.Split(host, " ")
-					if parts[len(parts)-1] == "REACHABLE" || parts[len(parts)-1] == "DELAY" {
-						onL2 = true
-					}
-				}
-			}
-
-			if onL2 {
-				log.Println("Accepted forward", dhost, dport)
-				return true
-			} else {
-				log.Println("Rejected forward: ", dhost, "is not on local L2")
-				return false
-			}
-
-		}),
 		Addr: ":2222",
-		Handler: ssh.Handler(func(s ssh.Session) {
-			io.WriteString(s, "Local forwarding available...\n")
-			select {}
-		}),
 		ChannelHandlers: map[string]ssh.ChannelHandler{
-			"session":      ssh.DefaultSessionHandler,
-			"direct-tcpip": ssh.DirectTCPIPHandler,
+			"direct-tcpip": customDirectHandler,
 		},
 	}
 
