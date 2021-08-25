5d1db579969ec0f5f5365fb25cef81d61eca0c77
Hampton Moore <me@hampton.pw>
2021-08-25 19:26:05 -0400

    Reformat so it works with -J

--- l2portal.go
+++ l2portal.go
@@ -13,21 +13,10 @@ import (
 func main() {
 	log.Println("starting ssh server on port 2222...")
 
-	sessions := []ssh.Session{}
-
 	server := ssh.Server{
 		LocalPortForwardingCallback: ssh.LocalPortForwardingCallback(func(ctx ssh.Context, dhost string, dport uint32) bool {
-			var user_session ssh.Session
-			for _, session := range sessions {
-				if session.Context() == ctx {
-					user_session = session
-					log.Printf("FOUND SESSION")
-				}
-			}
-			if user_session == nil {
-				log.Printf("NO SESSION FOUND")
-				return false
-			}
+			log.Println("SESSION")
+			log.Println(dhost, dport)
 
 			onL2 := false
 
@@ -39,7 +28,6 @@ func main() {
 			pinger.Timeout = 1
 			err = pinger.Run() // blocks until finished
 			if err != nil {
-				io.WriteString(user_session, "Failed to access host "+dhost+"\n")
 				return false
 			}
 			stats := pinger.Statistics()
@@ -49,8 +37,6 @@ func main() {
 			if err != nil {
 				panic(err)
 			}
-			log.Println(string(out))
-			log.Println(stats.IPAddr.IP.String())
 			hosts := strings.Split(string(out), "\n")
 			for _, host := range hosts {
 				if strings.Contains(host, stats.IPAddr.IP.String()) {
@@ -66,14 +52,12 @@ func main() {
 				return true
 			} else {
 				log.Println("Rejected forward: ", dhost, "is not on local L2")
-				io.WriteString(user_session, "Rejected forward: "+dhost+" is not on local L2\n")
 				return false
 			}
 
 		}),
 		Addr: ":2222",
 		Handler: ssh.Handler(func(s ssh.Session) {
-			sessions = append(sessions, s)
 			io.WriteString(s, "Local forwarding available...\n")
 			select {}
 		}),
